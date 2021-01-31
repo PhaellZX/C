74997bf9d90c02f6824e3789ccac67812e5f296a
Raphael <48337836+PhaellZX@users.noreply.github.com>
2021-01-31 00:36:04 -0300

    Update Desafio.cpp

--- Semana 01 - Atividades ED/Desafio.cpp
+++ Semana 01 - Atividades ED/Desafio.cpp
@@ -1,13 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// DESAFIO - Faça um programa que leia o arquivo da Contituição de 88
+// DESAFIO - FaÃ§a um programa que leia o arquivo da ContituiÃ§Ã£o de 88
 // e contar quantas palavras que tem o arquivo
-
-// Obs: Essa tive que dá uma pesquisada na web, pelo que eu entendi deve criar uma variável c(letra)
-// que vai por letra e letra no arquivo e passa por uma condição de se letra for igual ao 'espaço vazio'
-// OU letra é igual a 'quebra de linha' ele pula, senão conta +1, e uma outra observação coloquei o 
-// count = 1 porque parece que no C entendi o inicio como 0 nas contagens :/
 
 int main(){
 	
